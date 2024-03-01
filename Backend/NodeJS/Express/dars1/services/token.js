1a17067ad353b9b699d29491a43af15b2dcdfc0b
suhrob <suhrobrahmatullayev973132@gmail.com>
2024-03-01 15:43:02 +0500

    salom

--- Backend/NodeJS/Express/dars1/services/token.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateJWTToken = userId => {
-    const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' })
-    return accessToken
-}
-
-export { generateJWTToken }
